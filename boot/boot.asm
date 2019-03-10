487e0a359a5325a7c281b0c594263921d34992f4
alexs <34484473+AlekseySav@users.noreply.github.com>
2019-03-10 13:20:55 +0300

    Updaed main bootloader

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,144 +1,14 @@
-    jmp start
-
-cursor:
-    push ax
-    push bx
-    push cx
-    mov ah, 0x03
-    xor bh, bh
-    int 0x10
-    pop cx
-    pop bx
-    pop ax
-    ret
-
-puts:
-    pusha
-    call cursor
-    mov ax, 0x1301
-    xor bh, bh
-    int 0x10
-    popa
-    ret
-
-putc:
-    push ax
-    mov ah, 0x0e
-    int 0x10
-    pop ax
-    ret
-
-putl:
-    push ax
-    mov al, 13
-    call putc
-    mov al, 10
-    call putc
-    pop ax
-    ret
-
-pdone:
-    pusha
-    push bp
-    mov al, ' '
-    call putc
-    mov al, '['
-    call putc
-    mov bp, done
-    mov bx, 0x02
-    mov cx, 6
-    call puts
-    mov al, ']'
-    call putc
-    call putl
-    pop bp
-    popa
-    ret
-
-perr:
-    pusha
-    push bp
-    call putl
-    mov bx, 0x04
-    mov cx, 1
-    mov bp, perr.err
-    call puts
-    mov bp, err
-    mov bx, 0x07
-    mov cx, 23
-    call puts
-    call putl
-    pop bp
-    popa
-    ret
-
-getc:
-    push bx
-    mov bx, 0x07
-    xor ah, ah
-    int 0x16
-    or al, al
-    jz getc.end
-    call putc
-getc.end:
-    pop bx
-    ret
-
-readk:
-    call putl
-    pusha
-    push bp
-    mov bx, 0x07
-    mov cx, 18
-    mov bp, msg2
-    call puts
-    pop bp
-    popa
-
-    pusha
-    xor ah, ah
-    xor dx, dx
-    int 0x13
-    popa
-    jc die
-
-    pusha
-    push es
-    push di
-    mov ax, 0x0800
-    mov es, ax
-    xor di, di
-    mov ah, 0x02
-    mov al, 17
-    xor bx, bx
-    xor ch, ch
-    mov cx, 0x02
-    xor dx, dx
-    int 0x13
-    pop di
-    pop es
-    popa
-    jc die
-
-    call pdone
-    mov ax, 0x0800
-    ret
-
-killm:    
-    push dx 
-    push ax 
-    mov dx, 0x3f2 
-    xor al, al 
-    out dx, al 
-    pop ax 
-    pop dx 
-    ret
+BOOTSEG     equ 0x07c0 ; BIOS loads boot here
+INITSEG     equ 0x9000
+SYSSEG      equ 0x0060 ; First free memory
+MAGIC       equ 0x01fe ; MAGIC (0xAA55) location
+SECTORS     equ 15     ; sizeof kernel
+TRACKSIZE   equ 15     ; can't be more than 15
 
 start:
-    cli
-    mov ax, 0x07c0
+    mov ax, BOOTSEG
     mov ds, ax
-    mov ax, 0x0600
+    mov ax, INITSEG
     mov es, ax
     mov cx, 0x100
     xor di, di
@@ -146,8 +16,7 @@ start:
     cld
     rep
     movsw
-    sti
-    jmp 0x0600:go
+    jmp INITSEG:go
 go:
     cli
     mov ax, cs
@@ -159,49 +28,104 @@ go:
     mov sp, 0x400
     sti
 
-    call putl
-    call putl
-    mov bx, 0x07
-    mov cx, 37
-    mov bp, msg1
-    call puts
-    call putl
-sleep:
-    call getc
-    cmp ah, 0x1c
-    jne sleep
+    mov si, msg1
+    call print
 
+    mov ax, SYSSEG
     call readk
 
-    mov bx, 0x07
-    mov cx, 19
-    mov bp, msg3
-    call puts
+    mov si, msg2
+    call print
 
-    call killm
-    jc die
-    cmp ax, 0x0800
+    mov dx, 0x03f2
+    mov al, 0x0c
+    out dx, al      ; kill monitor
+
+    mov ah, 0x03
+    xor bh, bh
+    int 0x10
+    mov word [0x510], dx
+
+    cli
+
+    jmp SYSSEG:0
+    jmp die
+
+
+;
+; read kernel function
+; input: ax - kernel new place
+; ax must be #SYSSEG
+;
+readk:
+    cmp ax, SYSSEG
     jne die
-    call pdone
-    jmp 0x0800:0
+    
+    push ax
+    xor ax, ax
+    int 0x13    ; reload disk
+    pop ax
+    jc die
+
+    mov es, ax
+    xor bx, bx
+    mov ah, 0x02
+    mov al, 0x01
+    mov cx, TRACKSIZE
+    xor dx, dx
+    int 0x13    ; check, that there're TRACKSIZE sectors/track
+    jc die
+    
+    mov ah, 0x02
+    mov al, SECTORS
+    mov cx, 0x02
+    xor dx, dx
+    xor bx, bx
+    int 0x13
+    jc die
+    ret
+
+;
+; die partition
+; jmp here, if booting failed
+;
 die:
-    call perr
+    mov si, err
+    call print
+    cli
     hlt
 end:
     jmp $
+    
+;
+; print function
+; input: si - msg to print
+; must have zero at the end
+; output: si - end of msg
+;
+print:
+    push ax
+    push bx
+    mov ah, 0x0e
+    xor bh, bh
+    mov bl, 0x07
+print.n:
+    lodsb
+    or al, al
+    jz print.e
+    int 0x10
+    jmp print.n
+print.e:
+    pop bx
+    pop ax
+    ret
 
 msg1:
-    db "MUNIX 0.0.1 (Press Enter to continue)"
+    db 13, 10, 13, 10, "Munix 0.0.1 loading...", 13, 10, 0
 msg2:
-    db "Loading system ..."
-msg3:
-    db "Confirm changes ..."
-done:
-    db " done "
-perr.err:
-    db "E"
+    db "Booting system...", 0
 err:
-    db ": Failed booting kernel"
+    db 13, 10, "E: Failed booting Munix", 13, 10, 0
 
-times 510 - ($ - $$) db 0
-dw 0xaa55
+    times MAGIC - ($ - $$) db 0
+    dw 0xaa55
