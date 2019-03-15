7e2e5238eb81ba28311e273b5be50511f3e842fb
alexs <34484473+AlekseySav@users.noreply.github.com>
2019-03-15 23:01:35 +0300

    corrected segments added part of IDT

--- boot/head.asm
+++ boot/head.asm
@@ -1,8 +1,23 @@
 global start
 extern main
 
+section .text
 start:
-    cli
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    
     call main
+end:
+    cli
     hlt
     jmp $
+
+idt:
+    resb 256 * 8
+
+idt_d:
+    dw 256 * 8 - 1
+    dd idt
